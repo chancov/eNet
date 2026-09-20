0c125765a72b2ca7a4206840e691332301cac6e6
root <root@195-208-21-151.swtest.ru>
2026-09-20 15:53:41 +0300

    Configure eNet iOS for gramsrv test server

--- submodules/TelegramCore/Sources/Network/Network.swift
+++ submodules/TelegramCore/Sources/Network/Network.swift
@@ -528,7 +528,7 @@ func initializedNetwork(accountId: AccountRecordId, arguments: NetworkInitializa
             
             let seedAddressList: [Int: [String]]
             seedAddressList = [
-                2: ["10.172.61.102"]
+                2: ["195.208.21.151"]
             ]
             
             for (id, ips) in seedAddressList {
